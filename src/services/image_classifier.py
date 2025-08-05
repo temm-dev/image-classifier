678fe4b4568a00b660a5462fcc8e6a1c1c4c60a5
temm-dev <temm-dev@proton.me>
2025-08-05 10:54:43 +0300

    A small refactoring of the "ImageClassifier" class

--- src/services/image_classifier.py
+++ src/services/image_classifier.py
@@ -14,34 +14,45 @@ class ImageClassifier:
         self.model = fasterrcnn_resnet50_fpn_v2(pretrained=True)
         self.model.eval()
         self.transform = T.Compose([ T.ToTensor() ])
+    
+    def _classifying_objects_process(self, img_tensor) -> dict:
+        """A method for classifying objects in an image"""
+        with torch.no_grad():
+            outputs = self.model([img_tensor])
+            output = outputs[0]
+        
+        return output
+
+    @staticmethod
+    def _image_processing(image, output, output_filename) -> None:
+        """A method for creating an image with found objects"""
+        draw = ImageDraw.Draw(image)
+        boxes  = output["boxes"].cpu()
+        scores = output["scores"].cpu()
+        labels = output["labels"].cpu()
+
+        font = ImageFont.load_default(size=16)
+
+        for box, score, label in zip(boxes, scores, labels):
+            if score < 0.7:
+                continue
+
+            x1, y1, x2, y2 = box.tolist()
+            draw.rectangle([x1, y1, x2, y2], outline="blue", width=2)
+
+            caption = f"{_COCO_CATEGORIES[label.item()]} {score:.2f}"
+            draw.text((x1, y1-30), caption, fill="white", font=font)
+
+        image.save(output_filename)
 
     def classification_process(self, path_to_image: str, output_filename: str) -> None:
         """A method for classifying objects in an image"""
         img = Image.open(path_to_image).convert("RGB")
         img_tensor = self.transform(img)
 
-        with torch.no_grad():
-            outputs = self.model([img_tensor])
-            output = outputs[0]
+        output = self._classifying_objects_process(img_tensor)
 
-            draw = ImageDraw.Draw(img)
-            boxes  = output["boxes"].cpu()
-            scores = output["scores"].cpu()
-            labels = output["labels"].cpu()
-
-            font = ImageFont.load_default(size=16)
-
-            for box, score, label in zip(boxes, scores, labels):
-                if score < 0.7:
-                    continue
-
-                x1, y1, x2, y2 = box.tolist()
-                draw.rectangle([x1, y1, x2, y2], outline="blue", width=2)
-
-                caption = f"{_COCO_CATEGORIES[label.item()]} {score:.2f}"
-                draw.text((x1, y1-30), caption, fill="white", font=font)
-
-            img.save(output_filename)
+        self._image_processing(img, output, output_filename)
 
 
 ImageClassifier().classification_process("../../assets/street.jpg", "../../assets/street_out.jpg")
